b7b8896fd015550eba303cb1c4869be2ce8acb84
Shantanu Sonawane <shantanusonawane15@gmail.com>
2023-10-06 11:30:54 +0530

    First Commit

---
+++ src/test/java/com/jenkinsjobs/Jenkins1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
