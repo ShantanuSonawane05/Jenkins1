04420670cc5583486956e311fc880fbee493ff21
Shantanu Sonawane <131355356+ShantanuSonawane05@users.noreply.github.com>
2023-10-06 12:12:21 +0530

    Update Jenkins1Application.java

--- src/main/java/com/jenkinsjobs/Jenkins1Application.java
+++ src/main/java/com/jenkinsjobs/Jenkins1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jenkins1Application {
 
 	public static void main(String[] args) {
-		System.out.println("Project is Runnig");
+		System.out.println("Project is Runnig with Maven Build");
 		SpringApplication.run(Jenkins1Application.class, args);
 	}
 
